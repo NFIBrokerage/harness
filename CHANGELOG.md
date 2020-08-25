58eae3c22197d6edfc14fa09601a2aea995511cf
Michael Davis <mcarsondavis@gmail.com>
2020-08-25 16:37:58 -0500

    inspect_or_interpolate/1 usable in templates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a
 Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 0.1.2 - 2020-08-25
+
+### Added
+
+- A function to interpolate or inspect items into a template
+
 ## 0.1.1 - 2020-08-25
 
 ### Added
--- lib/harness/renderer/helpers.ex
+++ lib/harness/renderer/helpers.ex
@@ -1,8 +1,24 @@
 defmodule Harness.Renderer.Helpers do
   @moduledoc """
   Helper functions for renders
+
+  These functions are accessible inside templates.
   """
 
+  @doc """
+  Converts a string or atom into PascalCase
+
+  Makes use of the `Macro.camelize/1` function underneath.
+  """
   def pascal(item) when is_binary(item), do: Macro.camelize(item)
   def pascal(item) when is_atom(item), do: item |> to_string |> pascal()
+
+  @doc """
+  Either inspects an item or interpolates it.
+
+  If the item is a binary, it is interpolated. Else, it is inspected with
+  `Kernel.inspect/2` and then interpolated.
+  """
+  def inspect_or_interpolate(item) when is_binary(item), do: item
+  def inspect_or_interpolate(item), do: Kernel.inspect(item, limit: :infinity)
 end
