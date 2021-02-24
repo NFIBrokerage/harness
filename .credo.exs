154d964d85ccdf128388d020c919d35badfbd478
Tony van Riet <tony.van.riet@nfiindustries.com>
2021-02-24 11:20:53 -0600

    Customize credo config

--- .credo.exs
+++ .credo.exs
@@ -41,7 +41,7 @@
       # If you create your own checks, you must specify the source files for
       # them here, so they can be loaded by Credo before running the analysis.
       #
-      requires: [],
+      requires: ["deps/convene/lib/convene/check/*.ex"],
       #
       # If you want to enforce a style guide and need a more traditional linting
       # experience, you can change `strict` to `true` below:
@@ -131,7 +131,7 @@
         {Credo.Check.Refactor.NegatedConditionsWithElse, []},
         {Credo.Check.Refactor.Nesting, []},
         {Credo.Check.Refactor.UnlessWithElse, []},
-        {Credo.Check.Refactor.WithClauses, []},
+        {Credo.Check.Refactor.WithClauses, false},
 
         #
         ## Warnings
